8754803e9161a4f703028c4376335c22936d4819
JaeyeonKIM <foodsksms@gmail.com>
2018-01-24 22:50:06 +0900

    java basic study generator, this keyword

--- src/main/java/CarExam.java
+++ src/main/java/CarExam.java
@@ -1,11 +1,9 @@
 public class CarExam {
     public static void main(String [] args){
-        Car c1 = new Car();
-        Car c2 = new Car();
+        Car c1 = new Car("소방차");
+        Car c2 = new Car("구급차");
 
-        c1.name = "소방차";
         c1.number = 1234;
-        c2.name = "구급차";
         c2.number = 1111;
 
         System.out.println(c1.name);
---
+++ src/main/java/CarExam2.java
@@ -0,0 +1,7 @@
+public class CarExam2 {
+    public static void main(String [] args){
+        Car c1 = new Car("carName");
+        System.out.println(c1.name);
+
+    }
+}
