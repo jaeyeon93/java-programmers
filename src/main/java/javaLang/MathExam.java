1da7045e8ffd75a1332cb684ba2ce351c6a03900
jimmy <foodsksms@gmail.com>
2018-02-01 16:09:15 +0900

    Study java.lang package

---
+++ src/main/java/javaLang/MathExam.java
@@ -0,0 +1,17 @@
+package javaLang;
+
+public class MathExam {
+    public static void main(String [] args){
+        int value1 = Math.max(5, 30);
+        System.out.println(value1);
+        int value2 = Math.min(5, 30);
+        System.out.println(value2);
+        System.out.println(Math.abs(-10));
+
+        System.out.println(Math.random());
+        // 0이상 1.0미만의 실수값을 리턴해준다.
+
+        System.out.println(Math.sqrt(25));
+
+    }
+}
