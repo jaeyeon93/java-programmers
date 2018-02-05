16e8e7dd9b39115ef3e03143079a472ff6fa8886
jimmy <foodsksms@gmail.com>
2018-02-05 15:31:56 +0900

    Sentence program step2 finsih

--- src/main/java/sentence/Sentence.java
+++ src/main/java/sentence/Sentence.java
@@ -1,26 +1,25 @@
 package sentence;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Sentence {
-    static List<String> reverseList;
-
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("입력");
         String s = scanner.nextLine();
-        System.out.println(s);
         System.out.println("문장의 길이 : " + s.length());
         int length = s.length();
         String s2 = s.substring(0, length - 1);
-        System.out.println(s2);
         List<String> list = makeList(s2);
-        printList(list);
+        //printList(list);
         System.out.println("거꾸로 출력");
-//        List<String> reverse = reverse(list);
-//        printReverse(reverse);
+        printReverse(list);
+        System.out.println();
+        System.out.println("전체 수");
+        countWords(s2);
     }
 
 
@@ -29,23 +28,23 @@ public class Sentence {
         return list;
     }
 
-    public static void printList(List<String> list){
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
+//    public static void printList(List<String> list){
+//        for(int i = 0; i < list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
+//    }
+
+    public static void printReverse(List<String> list){
+        for(int i = list.size() - 1; i > 0; i--){
+            System.out.print(list.get(i) + " ");
         }
+        System.out.print("?");
     }
 
-//    public static List<String> reverse(List<String> list){
-//        for(int i = list.size(); i > 0; i--){
-//            reverseList.add(list.get(i));
-//        }
-//        return reverseList;
-//    }
-//
-//    public static void printReverse(List<String> reverse){
-//        for(int i = 0; i < reverse.size(); i++){
-//            System.out.println(reverseList.get(i));
-//        }
-//    }
+    public static void countWords(String s){
+        s = s.replace(" ", "");
+        System.out.println("sum is " + s.length());
+
+    }
 
 }
