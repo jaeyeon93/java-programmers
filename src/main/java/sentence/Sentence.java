090bc981c0c994cd9756156000cb85c2a28ab951
jimmy <foodsksms@gmail.com>
2018-02-09 15:18:05 +0900

    sentence project make alphabet list using hashMap

--- src/main/java/sentence/Sentence.java
+++ src/main/java/sentence/Sentence.java
@@ -1,30 +1,31 @@
 package sentence;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Sentence {
-    static ArrayList<String> result;
+    static List<String> result = new ArrayList<>();
+    static List<Integer> count = new ArrayList<>();
+    static HashMap<String, Integer> map = new HashMap<String, Integer>();
 
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("입력");
         String s = scanner.nextLine();
-        //System.out.println("문장의 길이 : " + s.length());
         int length = s.length();
         String s2 = s.substring(0, length - 1);
         List<String> list = makeList(s2);
-        System.out.println("거꾸로 출력");
         printReverse(list);
         System.out.println();
-        System.out.println("전체 수");
         countWords(s2);
         System.out.println();
         List<String> obj = alphabet(s2);
-        alphabetList(obj);
-        printResult(result);
+        print(obj);
+        System.out.println();
+        removeOverlap(obj);
+        List<Integer> count = counting(obj);
+        printCount(count);
+        map = numberOfAlphabet(obj);
+        System.out.println(map);
     }
 
 
@@ -43,7 +44,7 @@ public class Sentence {
 
     public static void countWords(String s){
         s = s.replace(" ", "");
-        System.out.println("sum is " + s.length());
+        System.out.println("전체 수 : " + s.length());
     }
 
     public static List<String> alphabet(String s){
@@ -52,21 +53,48 @@ public class Sentence {
         return obj;
     }
 
-    public static ArrayList<String> alphabetList(List <String> obj){
-        for(int i = 0; i < obj.size(); i++){
-//            if(result.contains(obj.get(i))){
-//                continue;
-//            } else {
-//                result.add(obj.get(i));
-//            }
-            result.add(obj.get(i));
+    public static void print(List<String> list){
+        for(int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i) + " ");
         }
+    }
+
+    public static List<String> removeOverlap(List<String> obj){
+        for(int i = 0; i < obj.size() -1; i++){
+                if(result.contains(obj.get(i))){
+                    continue;
+                } else {
+                    result.add(obj.get(i));
+                }
+            }
         return result;
     }
 
-    public static void printResult(List<String> result){
+    public static List<Integer> counting(List<String> obj){
+
         for(int i = 0; i < result.size(); i++){
-            System.out.print(result.get(i) + " ");
+            int number = 0;
+            for(int j = 0; j < obj.size(); j++){
+                if(result.get(i) == obj.get(j)){
+                    number++;
+                }
+            }
+            count.add(number);
+        }
+        return count;
+    }
+
+    public static void printCount(List<Integer> count){
+        for(int i = 0; i < count.size(); i++){
+            System.out.print(count.get(i) + " ");
         }
     }
+
+    public static HashMap<String, Integer> numberOfAlphabet(List<String> obj){
+        for(int i = 0; i < obj.size(); i++){
+            map.put(obj.get(i), 0);
+        }
+        return map;
+    }
+
 }
