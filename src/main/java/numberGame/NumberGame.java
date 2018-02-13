2fb5117739e4fc62e8430874cf2694420d6387b3
jimmy <foodsksms@gmail.com>
2018-02-13 11:08:34 +0900

    숫자만들기게임 최종완료

--- src/main/java/numberGame/NumberGame.java
+++ src/main/java/numberGame/NumberGame.java
@@ -3,26 +3,32 @@ package numberGame;
 import java.util.Scanner;
 
 public class NumberGame {
+    static int input;
+
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("숫자 맞추기 게임에 오신 것을 환영합니다.");
         System.out.println("숫자를 입력해주세요 (1-100)");
         int random = (int)Math.floor(Math.random() * 100 + 1);
-        int input = scanner.nextInt();
+
         int count = 0;
         while (input != random){
-            int input2 = scanner.nextInt();
-            if (input2 > random) {
-                System.out.println(input2 + "보다 작아요.");
+            int input = scanner.nextInt();
+
+            if (input > random) {
+                System.out.println(input + "보다 작아요.");
             } else {
-                System.out.println(input2 + "보다 커요.");
+                System.out.println(input + "보다 커요.");
             }
 
-            if(input2 == random)
+            if(input == random){
+                count++;
                 break;
+            }
             count++;
         }
+
         System.out.println(count+"번만에 맞췄습니다!");
-        System.out.println(random);
+        System.out.println("플레이해 주셔서 감사합니다.");
     }
 }
