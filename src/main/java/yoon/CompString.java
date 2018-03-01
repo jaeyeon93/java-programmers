45843c63f4a79adc151b14dbc24ccb3568cb0378
JaeyeonKIM <foodsksms@gmail.com>
2018-03-01 21:18:56 +0900

    study 열혈java chapter11 string class stringBuilder

---
+++ src/main/java/yoon/CompString.java
@@ -0,0 +1,28 @@
+package yoon;
+
+public class CompString {
+    public static void main(String [] args){
+        String str1 = "Lexicographically";
+        String str2 = "lexicographically";
+        int cmp;
+
+        if(str1.equals(str2))
+            System.out.println("두 문자열은 같습니다.");
+        else
+            System.out.println("두 문자열은 다릅니다.");
+
+        cmp = str1.compareTo(str2);
+
+        if(cmp == 0)
+            System.out.println("두 문자열은 일치합니다.");
+        else if (cmp < 0)
+            System.out.println("사전의 앞에 위치하는 문자 : " + str1);
+        else
+            System.out.println("사전의 앞에 위치하는 문자 : " + str2);
+
+        if(str1.compareToIgnoreCase(str2) == 0)
+            System.out.println("두 문자열은 같습니다.");
+        else
+            System.out.println("두 문자열은 다릅니다.");
+    }
+}
---
+++ src/main/java/yoon/SubString.java
@@ -0,0 +1,12 @@
+package yoon;
+
+public class SubString {
+    public static void main(String [] args){
+        String str1 = "abcdefg";
+        String str2 = str1.substring(2);
+        System.out.println(str2);
+
+        String str3 = str1.substring(2, 4);
+        System.out.println(str3);
+    }
+}
