ac115df92014e4c4f10f7715bd0c1ec619d70517
JaeyeonKIM <foodsksms@gmail.com>
2018-03-02 14:11:44 +0900

    study 열혈java chapter12 console part

---
+++ src/main/java/yoon/FormatString.java
@@ -0,0 +1,12 @@
+package yoon;
+
+public class FormatString {
+    public static void main(String [] args){
+        int age = 20;
+        double height = 178.2;
+        String name = "kim jimmy";
+
+        System.out.printf(" name : %s \n", name);
+        System.out.printf(" age : %d \n height : %e \n\n", age, height);
+    }
+}
