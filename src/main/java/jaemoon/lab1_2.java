42e3372ca15e66608f89694c4c28fc701d5fadba
jimmy <foodsksms@gmail.com>
2018-03-03 12:30:02 +0900

    jaemoon's problem lab1 finish

---
+++ src/main/java/jaemoon/lab1_2.java
@@ -0,0 +1,19 @@
+package jaemoon;
+
+public class lab1_2 {
+    public static void main(String [] args){
+        int x = 5;
+        int y = 4;
+        double z = 4.0;
+
+        System.out.println(x/y*z); // result is 4.0
+        System.out.println(x*z/y); // result is 5.0
+
+        /*
+        1st
+        x/y => 1, 1*z = 4.0;
+        2nd
+        x*z => 20.0, 20.0/y = 5.0;
+         */
+    }
+}
---
+++ src/main/java/jaemoon/lab1_3.java
@@ -0,0 +1,11 @@
+package jaemoon;
+
+public class lab1_3 {
+    public static void main(String [] args){
+        int a = 5;
+        int x = 0;
+
+        x = (int) (a * a * Math.PI);
+        System.out.println(x);
+    }
+}
