224f14f25a3c9d50eeb1c04d13f93ace70729465
jimmy <foodsksms@gmail.com>
2018-03-16 14:45:13 +0900

    열혈java enum

---
+++ src/main/java/yoon/EnumParamConstructor.java
@@ -0,0 +1,20 @@
+package yoon;
+
+enum Person2 {
+    MAN(29);
+
+    int age;
+    private Person2(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "I am " + age + " years old";
+    }
+}
+
+public class EnumParamConstructor {
+    public static void main(String [] args) {
+        System.out.println(Person2.MAN);
+    }
+}
