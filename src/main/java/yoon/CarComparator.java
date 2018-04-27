c5483fb59472b385f183fe531f8de4c2746d1888
JaeyeonKIM <foodsksms@gmail.com>
2018-04-27 23:53:48 +0900

    java collection frames work, search sorting

---
+++ src/main/java/yoon/CarComparator.java
@@ -0,0 +1,65 @@
+package yoon;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+class Car {
+    protected int disp;
+    public Car(int d) { disp = d; };
+
+    @Override
+    public String toString() { return "cc:" + disp; };
+}
+
+class CarComp implements Comparable<Car> {
+
+//    @Override
+//    public int compare(Car o1, Car o2) { return o1.disp - o2.disp; }
+
+    @Override
+    public int compareTo(Car o) {
+        return 0;
+    }
+}
+
+class ECar extends Car {
+    private int battery;
+
+    public ECar(int d, int b) {
+        super(d);
+        battery = b;
+    }
+
+    @Override
+    public String toString() { return "cc: " + disp + ", ba : " + battery; }
+}
+
+class CarComparator {
+    public static void main(String[] args) {
+        List<Car> clist = new ArrayList<>();
+        clist.add(new Car(1800));
+        clist.add(new Car(1200));
+        clist.add(new Car(3000));
+
+        List<ECar> elist = new ArrayList<>();
+        elist.add(new ECar(3000, 55));
+        elist.add(new ECar(1800, 87));
+        elist.add(new ECar(1200, 99));
+
+        CarComp comp = new CarComp();
+
+        // 각각정렬
+//        Collections.sort(clist, comp);
+//        Collections.sort(elist, comp);
+
+        for (Iterator<Car> itr= clist.iterator(); itr.hasNext(); )
+            System.out.println(itr.next().toString() + "\t");
+        System.out.println();
+
+        for (Iterator<ECar> itr = elist.iterator(); itr.hasNext(); )
+            System.out.println(itr.next().toString() + "\t");
+        System.out.println();
+    }
+}
