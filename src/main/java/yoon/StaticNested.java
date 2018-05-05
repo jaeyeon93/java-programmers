316a7516cc20487c508968300a8a5b4bd8dd527a
JaeyeonKIM <foodsksms@gmail.com>
2018-05-06 00:31:29 +0900

    Study java Nested class

---
+++ src/main/java/yoon/StaticNested.java
@@ -0,0 +1,20 @@
+package yoon;
+
+class Outer {
+    private static int num = 0;
+    static class Nested1 { // static 네스티드 클래스
+        void add(int n) { num += n; }
+    }
+    static class Nested2 { // Static 네스티드 클래스
+        int get() {return num;}
+    }
+}
+
+public class StaticNested {
+    public static void main(String[] args) {
+        Outer.Nested1 nst1 = new Outer.Nested1();
+        nst1.add(5);
+        Outer.Nested2 nst2 = new Outer.Nested2();
+        System.out.println(nst2.get());
+    }
+}
