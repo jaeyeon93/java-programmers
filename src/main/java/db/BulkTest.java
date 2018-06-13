38dced945dd2c14e9481e281fd69e8b34a50b216
JaeyeonKIM <foodsksms@gmail.com>
2018-06-14 00:04:19 +0900

    BulkTest로 임의의 데이터를 생성을 하고, 생성된 데이터를 csv파일에 직접 저장을 하는 코드 이러한 형태를 bulk insert라고한다.

--- src/main/java/db/BulkTest.java
+++ src/main/java/db/BulkTest.java
@@ -1,10 +1,48 @@
 package db;
 
-public class BulkTest {
-    /*
-    * a int, b varchar(16)
-    * */
-    public static void main(String[] args) {
+import java.util.Random;
 
+public class BulkTest {
+    private static Random r = new Random();
+
+    /*
+    mysql> desc btest;
+    +-------+-------------+------+-----+---------+-------+
+    | Field | Type        | Null | Key | Default | Extra |
+    +-------+-------------+------+-----+---------+-------+
+    | a     | int(11)     | NO   | PRI | NULL    |       |
+    | b     | varchar(16) | YES  |     | NULL    |       |
+    +-------+-------------+------+-----+---------+-------+
+    2 rows in set (0.00 sec)
+    */
+    public static void main(String[] args) {
+        BulkTest bulk = new BulkTest();
+        int count = Integer.parseInt(args[0]);
+        bulk.getData(count, 16);
+    }
+
+    private void progress(int curr, int tot) {
+        if (tot < 1000)
+            return;
+        if (curr % (tot / 10) == 0) {
+            System.err.print("*");
+            return;
+        }
+        if (curr == tot - 1)
+            System.err.println("");
+    }
+
+    private void getData(int count, int charlen) {
+        for (int i = 0; i < count; i++) {
+            progress(i, count);
+            System.out.printf("%d,%s\n", i, genStr(charlen));
+        }
+    }
+
+    private Object genStr(int charlen) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < charlen; i++)
+            sb.append((char)('a' + r.nextInt(24)));
+        return sb.toString();
     }
 }
