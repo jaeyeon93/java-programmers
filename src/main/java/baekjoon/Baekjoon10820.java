dc1a71548e22aa17ec4ee2a1bc20a811e4a1e8d9
jaeyeon kim <foodsksms@gmail.com>
2018-09-10 10:35:55 +0900

    Solving Baekjoon10820

---
+++ src/main/java/baekjoon/Baekjoon10820.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10820 {
+    public static void counting(String str) {
+        String [] arr = str.split("");
+        int lower = 0;
+        int upper = 0;
+        int number = 0;
+        int space = 0;
+        for (int i  = 0; i < arr.length; i++) {
+            int converting = (int)(arr[i].charAt(0));
+            if (converting >= 48 && converting <= 57)
+                number++;
+            if (converting >= 65 && converting <= 90)
+                upper++;
+            if (converting >= 97 && converting <= 122)
+                lower++;
+            if (converting == 32)
+                space++;
+        }
+        System.out.println(lower + " " + upper + " " + number + " " + space);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext())
+            counting(sc.nextLine());
+    }
+}
