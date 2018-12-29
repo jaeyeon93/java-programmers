70f63b95388618342ec3a31e9f736779ab1c8b54
jaeyeon kim <foodsksms@gmail.com>
2018-12-29 14:05:05 +0900

    Solving Baekjoon8958

--- src/main/java/baekjoon/Baekjoon8958.java
+++ src/main/java/baekjoon/Baekjoon8958.java
@@ -5,27 +5,26 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Baekjoon8958 {
-    public static Integer score(String input) {
+    public static Integer solution(String input) {
         List<String> arr = Arrays.asList(input.split(""));
         int sum = 0;
-        int score = 1;
-        for (int i = 0; i < arr.size()-1; i++) {
-            System.out.println(i + "번째 score : " + score);
-            if (arr.get(i+1).equals("O")) {
+        int score = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i).equals("O")) {
                 ++score;
+                sum += score;
             }
 
-            if (arr.get(i+1).equals("X")) {
+            if (arr.get(i).equals("X"))
                 score = 0;
-            }
-            sum += score;
         }
         return sum;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = score(sc.nextLine());
-        System.out.println(result);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++)
+            System.out.println(solution(sc.next()));
     }
 }
