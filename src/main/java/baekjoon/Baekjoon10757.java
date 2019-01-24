2832ede1b594dc1e22db21cab531e68f78ddeb73
jaeyeon kim <foodsksms@gmail.com>
2019-01-24 15:06:43 +0900

    Solv baekjoon10757

---
+++ src/main/java/baekjoon/Baekjoon10757.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Baekjoon10757 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+        System.out.println(n.add(m));
+    }
+}
