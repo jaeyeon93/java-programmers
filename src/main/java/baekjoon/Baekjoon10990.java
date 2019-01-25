49327485a33766416306efdc0e56e2e4c71451fd
jaeyeon kim <foodsksms@gmail.com>
2019-01-25 19:20:41 +0900

    Solv baekjoon10990

---
+++ src/main/java/baekjoon/Baekjoon10990.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10990 {
+    public static String star(int i, int n) {
+        StringBuilder sb = new StringBuilder();
+        if (i == 1) {
+            for (int j = 0; j < n-1; j++)
+                sb.append(" ");
+            sb.append("*");
+            return sb.toString();
+        }
+
+        for (int j = 0; j < n-i; j++) {
+            sb.append(" ");
+        }
+        sb.append("*");
+        for (int j = 0; j < 2*i-3; j++)
+            sb.append(" ");
+        sb.append("*");
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++)
+            System.out.println(star(i, n));
+    }
+}
