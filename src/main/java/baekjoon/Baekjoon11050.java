e1767c4df638ea09fe465528faf5c62cf512a59e
jaeyeon kim <foodsksms@gmail.com>
2019-01-29 23:09:42 +0800

    Solv baekjoon 11050

--- src/main/java/baekjoon/Baekjoon11050.java
+++ src/main/java/baekjoon/Baekjoon11050.java
@@ -1,4 +1,23 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon11050 {
+    public static Integer result(int n, int m) {
+        return factorial(n)/(factorial(m)*factorial(n-m));
+    }
+
+    public static Integer factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+            result *= i;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(result(n, m));
+    }
 }
