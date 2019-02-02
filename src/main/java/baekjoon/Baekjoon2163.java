98047e52a9c1d164b8015e10106b7ddd29d7e004
jaeyeon kim <foodsksms@gmail.com>
2019-02-02 22:07:48 +0900

    Solv baekjoon

--- src/main/java/baekjoon/Baekjoon2163.java
+++ src/main/java/baekjoon/Baekjoon2163.java
@@ -1,4 +1,17 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2163 {
+    public static Integer solution(int n, int m) {
+        int result = (m-1) + m*(n-1);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(solution(n, m));
+    }
 }
