b704c8c6f25151ad6e53fc88f68d6bd174edb8fa
jaeyeon kim <foodsksms@gmail.com>
2019-02-06 17:52:42 +0900

    Solv baekjoon 2523

--- src/main/java/baekjoon/Baekjoon2523.java
+++ src/main/java/baekjoon/Baekjoon2523.java
@@ -1,4 +1,24 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2523 {
+    public static String star(int n, int i) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < i; j++)
+            sb.append("*");
+        sb.append(" ");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(star(n, i));
+        }
+        for (int i = n-1; i > 0; i--) {
+            System.out.println(star(n,i));
+        }
+    }
 }
