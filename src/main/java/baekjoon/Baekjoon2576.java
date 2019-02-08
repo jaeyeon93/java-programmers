56af9292b7ad3988b32e2ac44c5992c386dd4e1d
jaeyeon kim <foodsksms@gmail.com>
2019-02-08 23:26:53 +0900

    Solv baekjoon2576

--- src/main/java/baekjoon/Baekjoon2576.java
+++ src/main/java/baekjoon/Baekjoon2576.java
@@ -1,4 +1,26 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Baekjoon2576 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            int n = sc.nextInt();
+            if (n % 2 != 0)
+                list.add(n);
+        }
+        if (list.size() == 0) {
+            System.out.println(-1);
+        } else {
+            int sum = list.stream().mapToInt(Integer::intValue).sum();
+            int min = Collections.min(list);
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
 }
