1a1e95ac4ec663e6d584a7b9dcc982b87935502a
jaeyeon kim <foodsksms@gmail.com>
2019-02-09 22:52:23 +0900

    Solv baekjoon2751

--- src/main/java/baekjoon/Baekjoon2751.java
+++ src/main/java/baekjoon/Baekjoon2751.java
@@ -1,18 +1,36 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Baekjoon2751 {
+    public static List<Integer> sort(List<Integer> list) {
+        if (list.size() < 2)
+            return list;
+
+        int pivot = list.get(0);
+        List<Integer> lower = new ArrayList<>();
+        List<Integer> higher = new ArrayList<>();
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < pivot) {
+                lower.add(list.get(i));
+            } else {
+                higher.add(list.get(i));
+            }
+        }
+        List<Integer> sorted = sort(lower);
+        sorted.add(pivot);
+        sorted.addAll(sort(higher));
+        return sorted;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
-        int [] arr = new int [count];
-        for (int i = 0; i < count; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < count; i++)
+            list.add(sc.nextInt());
+        List<Integer> result = sort(list);
+        for (Integer i : result)
+            System.out.println(i);
     }
+
 }
