62fbbc1b40f4c326074de7e4e228662cbd94b589
JaeyeonKIM <foodsksms@gmail.com>
2019-03-14 12:09:43 +0900

    Solve baekjoon2133

---
+++ src/main/java/baekjoon/Baekjoon2133.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2133 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] dp = new int[n+1];
+        dp[0] = 1;
+        dp[2] = 3;
+        for (int i = 4; i <= n; i += 2) {
+            dp[i] = dp[i-2]*3;
+            for (int j = 4; j <= i; j += 2)
+                dp[i] += dp[i-j]*2;
+        }
+        System.out.println(dp[n]);
+
+    }
+}
