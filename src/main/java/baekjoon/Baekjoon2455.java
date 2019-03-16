fe4f118620af7d2767e689388eda125118e9a6f3
jaeyeon kim <foodsksms@gmail.com>
2019-03-17 00:23:57 +0900

    Solve baekjoon 2455

--- src/main/java/baekjoon/Baekjoon2455.java
+++ src/main/java/baekjoon/Baekjoon2455.java
@@ -1,4 +1,18 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2455 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int current = 0;
+        int max = current;
+        for (int i = 0; i < 4; i++) {
+            current -= sc.nextInt();
+            current += sc.nextInt();
+            if (current > max)
+                max = current;
+        }
+        System.out.println(max);
+    }
 }
