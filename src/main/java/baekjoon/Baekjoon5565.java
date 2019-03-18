a0eed7b02b22e29e15f457ff6c4ba9323ffaf315
jaeyeon kim <foodsksms@gmail.com>
2019-03-18 16:46:16 +0900

    Solve baekjoon5565 algorithm

--- src/main/java/baekjoon/Baekjoon5565.java
+++ src/main/java/baekjoon/Baekjoon5565.java
@@ -1,4 +1,13 @@
 package baekjoon;
 
-public class Baekjoon55656 {
+import java.util.Scanner;
+
+public class Baekjoon5565 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        for (int i = 0; i < 9; i++)
+            total -= sc.nextInt();
+        System.out.println(total);
+    }
 }
