0d7fcbff42e3d0d7be8ed7e94d65a21e742a0cd3
jaeyeon kim <foodsksms@gmail.com>
2019-03-30 15:35:53 +0900

    Solve baekjoon1158

--- src/main/java/baekjoon/Baekjoon1158.java
+++ src/main/java/baekjoon/Baekjoon1158.java
@@ -1,4 +1,23 @@
 package baekjoon;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Baekjoon1158 {
+    public static List<Integer> solution(List<Integer> list, int n) {
+        int pop = list.get(n);
+        list.remove(n);
+        list.add(pop);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= 7; i++)
+            list.add(i);
+
+        for (int i = 0; i < 7; i++)
+            System.out.println(solution(list, 2));
+    }
 }
