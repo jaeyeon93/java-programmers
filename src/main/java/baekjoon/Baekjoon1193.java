da68f70457390f14223d59ad8e2a1460eba5e957
jaeyeon kim <foodsksms@gmail.com>
2019-03-31 00:31:13 +0900

    Solve baekjoon1193

--- src/main/java/baekjoon/Baekjoon1193.java
+++ src/main/java/baekjoon/Baekjoon1193.java
@@ -1,4 +1,32 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon1193 {
+    public static String solution(int n, int count, int sum, int i) {
+        StringBuilder sb = new StringBuilder();
+        sb.append((sum-i-1));
+        sb.append("/");
+        sb.append(i);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 2;
+        int count = 0;
+        while (count != n) {
+            for (int i = 1; i < sum-1; i++) {
+                solution(n, count, sum, i);
+                count++;
+                if (count == n) {
+                    System.out.println(solution(n, count, sum, i));
+                    break;
+                }
+            }
+            sum++;
+
+        }
+    }
 }
