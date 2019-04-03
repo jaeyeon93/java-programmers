8a5f8d3b6232a3cef6171faa76fc1b95e56cd54f
jaeyeon kim <foodsksms@gmail.com>
2019-04-03 19:15:31 +0900

    Solve baekjoon2193 using dp

--- src/main/java/baekjoon/Baekjoon2193.java
+++ src/main/java/baekjoon/Baekjoon2193.java
@@ -15,16 +15,24 @@ public class Baekjoon2193 {
 //        System.out.println(arr[num]);
 //    }
     public static void main(String[] args) throws IOException {
+        // 99%퍼센트에서 런타임에러난다
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String s = bf.readLine();
         int num = Integer.parseInt(s);
+        if (num == 1) {
+            bw.write(String.valueOf(1));
+            bw.flush();
+            bw.close();
+            return;
+        }
         long [] arr = new long[num+1];
         arr[0] = 1;
         arr[1] = 1;
         arr[2] = 1;
         for (int i = 3; i <= num; i++)
             arr[i] = arr[i-1] + arr[i-2];
+
         bw.write(String.valueOf(arr[num]));
         bw.flush();
         bw.close();
