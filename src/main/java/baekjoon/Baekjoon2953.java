f6ec0bfe97bd1e39c51169040ceca3951976dba8
jaeyeon kim <foodsksms@gmail.com>
2019-04-04 18:34:28 +0900

    Solve Baekjoon2953

--- src/main/java/baekjoon/Baekjoon2953.java
+++ src/main/java/baekjoon/Baekjoon2953.java
@@ -1,4 +1,21 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Baekjoon2953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            int sum = 0;
+            for (int j = 0; j < 4; j++)
+                sum += sc.nextInt();
+            list.add(sum);
+        }
+        int max = Collections.max(list);
+        System.out.println(list.indexOf(max)+1 + " " + max);
+    }
 }
