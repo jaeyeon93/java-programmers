15dcb3030144331721a1894008fbe443215b9f07
jaeyeon kim <foodsksms@gmail.com>
2019-04-06 17:46:31 +0900

    Solve baekjoon9020

--- src/main/java/baekjoon/Baekjoon9020.java
+++ src/main/java/baekjoon/Baekjoon9020.java
@@ -1,4 +1,38 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Baekjoon9020 {
+
+    public static boolean check(int n) {
+        for (int i = 2; i*i <= n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+    public static void solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i <= n; i++)
+            if (check(i))
+                list.add(i);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++)
+            for (int j = 0; j < list.size(); j++)
+                if (list.get(i) + list.get(j) == n) {
+                    System.out.println(list.get(i) + " " + list.get(j));
+                    return;
+                }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        solution(sc.nextInt());
+        for (int i = 0; i < n; i++)
+            solution(sc.nextInt());
+    }
 }
