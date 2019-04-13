9249a9f05bb673bcf8a8549453205bd32ef84b67
jaeyeon kim <foodsksms@gmail.com>
2019-04-13 17:55:54 +0900

    Solve Baekjoon1026

--- src/main/java/baekjoon/Baekjoon1026.java
+++ src/main/java/baekjoon/Baekjoon1026.java
@@ -1,4 +1,29 @@
 package baekjoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Baekjoon1026 {
+    public static Integer solution(int [] arr1, int [] arr2) {
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int sum = 0;
+        for (int i = 0; i < arr1.length; i++)
+            sum += arr1[i] * arr2[arr2.length-1-i];
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr1 = new int[n];
+        int [] arr2 = new int[n];
+        for (int i = 0; i < n; i++)
+            arr1[i] = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            arr2[i] = sc.nextInt();
+
+        System.out.println(solution(arr1, arr2));
+    }
 }
