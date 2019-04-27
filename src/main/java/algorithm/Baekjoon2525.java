aa0eb13a0114e9e3ef850d822e6cc8b3c117dea5
jaeyeon kim <foodsksms@gmail.com>
2019-04-27 22:09:18 +0900

    Solve baekjoon2525

--- src/main/java/algorithm/Baekjoon2525.java
+++ src/main/java/algorithm/Baekjoon2525.java
@@ -3,18 +3,29 @@ package algorithm;
 import java.util.Scanner;
 
 public class Baekjoon2525 {
-    public static void solution(int hour, int minute, int num) {
-        if (minute + num >= 60) {
-            hour += (minute + num) / 60;
-            if (hour >= 24)
-                System.out.println((hour - 24) + " " + (60 - minute));
-
-            if (hour < 24)
-                System.out.println((hour) + " " + (60-minute));
+    public static String solution(int hour, int minute, int num) {
+        int sum = minute+num;
+        StringBuilder sb = new StringBuilder();
+        if (sum > 59) {
+            int div = sum / 60;
+            int mod = sum % 60;
+            hour += div;
+            if (hour > 23) {
+                hour %= 24;
+                sb.append(hour);
+                sb.append(" ");
+                sb.append(mod);
+                return sb.toString();
+            }
+            sb.append(hour);
+            sb.append(" ");
+            sb.append(mod);
+            return sb.toString();
         }
-
-        if (minute + num < 60)
-            System.out.println((hour) + " " +(minute + num));
+        sb.append(hour);
+        sb.append(" ");
+        sb.append(sum);
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -22,6 +33,6 @@ public class Baekjoon2525 {
         int hour = sc.nextInt();
         int minute = sc.nextInt();
         int num = sc.nextInt();
-        solution(hour, minute, num);
+        System.out.println(solution(hour, minute, num));
     }
 }
