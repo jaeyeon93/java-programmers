22d751615735f8903028ab2013957774a88ac719
jaeyeon kim <foodsksms@gmail.com>
2019-04-28 00:11:41 +0900

    Solve baekjooon10699

--- src/main/java/baekjoon/Baekjoon10699.java
+++ src/main/java/baekjoon/Baekjoon10699.java
@@ -1,4 +1,11 @@
 package baekjoon;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Baekjoon10699 {
+    public static void main(String[] args) {
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        System.out.println(time);
+    }
 }
