9abd9c5b0a4f68402d93bdeaf08fde9a3af77f0e
jaeyeon kim <foodsksms@gmail.com>
2019-05-01 13:25:11 +0900

    Solve baekjoon10886

--- src/main/java/baekjoon/Baekjoon10886.java
+++ src/main/java/baekjoon/Baekjoon10886.java
@@ -1,4 +1,25 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon10886 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int people = sc.nextInt();
+        int non = 0;
+        int cute = 0;
+        for (int i = 0; i < people; i++) {
+            int num = sc.nextInt();
+            if (num == 0)
+                ++non;
+            else
+                ++cute;
+        }
+
+        if (non > cute) {
+            System.out.println("Junhee is not cute!");
+            return;
+        }
+        System.out.println("Junhee is cute!");
+    }
 }
