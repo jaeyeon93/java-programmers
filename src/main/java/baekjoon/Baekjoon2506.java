6106529a8a5de112ff85a67032ad4e10aa6948cf
jaeyeon kim <foodsksms@gmail.com>
2019-05-03 17:40:14 +0900

    Solve baekjoon2506

--- src/main/java/baekjoon/Baekjoon2506.java
+++ src/main/java/baekjoon/Baekjoon2506.java
@@ -1,4 +1,23 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2506 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 0; i < size-1; i++) {
+            if (arr[i] == 1 && arr[i+1] == 1)
+                sum += ++arr[i+1];
+            else
+                sum += arr[i];
+        }
+
+        System.out.println(sum);
+    }
 }
