4b2a8eac441f2f60b6337bd2b9e645017692d51a
jaeyeon kim <foodsksms@gmail.com>
2019-05-05 09:37:51 +0900

    Solve baekjoon9507

--- src/main/java/baekjoon/Baekjoon10819.java
+++
@@ -1,4 +0,0 @@
-package baekjoon;
-
-public class Baekjoon10819 {
-}
--- src/main/java/baekjoon/Baekjoon9507.java
+++ src/main/java/baekjoon/Baekjoon9507.java
@@ -1,4 +1,19 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon9507 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long [] arr = new long[68];
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 4;
+        for (int i = 4; i <= 67; i++)
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3] + arr[i-4];
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++)
+            System.out.println(arr[sc.nextInt()]);
+    }
 }
