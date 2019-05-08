cba22ee9f6ee61589199984a538fa17e856e7f87
jaeyeon kim <foodsksms@gmail.com>
2019-05-08 13:47:17 +0900

    solve baekjoon1357

--- src/main/java/baekjoon/Baekjoon1357.java
+++ src/main/java/baekjoon/Baekjoon1357.java
@@ -3,19 +3,30 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Baekjoon1357 {
-    public static Integer reverse(int n) {
-        StringBuilder sb = new StringBuilder();
-        String [] arr = String.valueOf(n).split("");
-        for (int i = arr.length-1; i >= 0; i--)
-            sb.append(arr[i]);
-        return Integer.parseInt(sb.toString());
+    public static Long convertDecimal(String str) {
+        long result = 0;
+        for (int i = str.length()-1; i >= 0; i--)
+            if (str.charAt(i) == '1')
+                result += Math.pow(2, str.length()-1-i);
+
+        return result;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int temp = reverse(a) + reverse(b);
-        System.out.println(reverse(temp));
+        String str = sc.next();
+        long result = convertDecimal(str);
+        StringBuilder sb = new StringBuilder();
+        while (result > 0) {
+            long temp = result % 8;
+            sb.append(temp);
+            result /= 8;
+        }
+        System.out.println(sb.reverse().toString());
     }
+    /*
+    110010101101100101011011001010110
+    62554533126
+
+     */
 }
