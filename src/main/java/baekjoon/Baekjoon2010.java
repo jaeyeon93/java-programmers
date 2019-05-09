a1259de6fd8814e9f02f4a35b370aa7fd0333ce0
jaeyeon kim <foodsksms@gmail.com>
2019-05-09 10:15:22 +0900

    Solve baekjoon2010

--- src/main/java/baekjoon/Baekjoon2010.java
+++ src/main/java/baekjoon/Baekjoon2010.java
@@ -1,4 +1,15 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int result = 0;
+        result += sc.nextInt();
+        for (int i = 0; i < num-1; i++)
+            result += (sc.nextInt()-1);
+        System.out.println(result);
+    }
 }
