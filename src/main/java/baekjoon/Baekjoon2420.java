5c978ffe57f6d8d07007a418fafe549dde0526b6
jaeyeon kim <foodsksms@gmail.com>
2019-05-10 01:49:07 +0900

    solve baekjoon2420

--- src/main/java/baekjoon/Baekjoon2420.java
+++ src/main/java/baekjoon/Baekjoon2420.java
@@ -1,4 +1,13 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long result = Math.abs(n - m);
+        System.out.println(result);
+    }
 }
