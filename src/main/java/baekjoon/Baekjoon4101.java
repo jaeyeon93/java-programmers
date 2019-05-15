6df5efdefb4248ab027d5d9d054ab5c660c7824c
jaeyeon kim <foodsksms@gmail.com>
2019-05-15 16:52:36 +0900

    Solve baekjoon4101

--- src/main/java/baekjoon/Baekjoon4101.java
+++ src/main/java/baekjoon/Baekjoon4101.java
@@ -1,4 +1,22 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon4101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int first = sc.nextInt();
+            int second = sc.nextInt();
+
+            if (first == 0 && second == 0)
+                break;
+
+            if (first > second)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+
+        }
+    }
 }
