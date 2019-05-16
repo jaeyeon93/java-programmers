36b8776c5bba949f5187bf3b93e037c024d13cc0
jaeyeon kim <foodsksms@gmail.com>
2019-05-16 15:15:21 +0900

    Solve baekjoon3047

--- src/main/java/baekjoon/Baekjoon3047.java
+++ src/main/java/baekjoon/Baekjoon3047.java
@@ -1,4 +1,24 @@
 package baekjoon;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Baekjoon3047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[3];
+        for (int i = 0; i < 3; i++)
+            arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", arr[0]);
+        map.put("B", arr[1]);
+        map.put("C", arr[2]);
+        String str = sc.next();
+        String [] split = str.split("");
+        for (String s : split)
+            System.out.print(map.get(s) + " ");
+    }
 }
