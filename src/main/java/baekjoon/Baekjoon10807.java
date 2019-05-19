bf64bff2b062367f44f45b09eae5a2641109b1f5
jaeyeon kim <foodsksms@gmail.com>
2019-05-19 23:36:07 +0900

    Solve baekjoon10807

--- src/main/java/baekjoon/Baekjoon10807.java
+++ src/main/java/baekjoon/Baekjoon10807.java
@@ -1,4 +1,19 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = sc.nextInt();
+        int match = sc.nextInt();
+        int count = 0;
+        for (Integer i : arr)
+            if (i == match)
+                count++;
+        System.out.println(count);
+    }
 }
