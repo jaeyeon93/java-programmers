5961eb7eb6ba6064b9e860c14769ddf91e2c404f
JaeyeonKIM <foodsksms@gmail.com>
2019-06-02 00:16:49 +0900

    Solve Baekjoon1026 another solution

--- src/main/java/baekjoon/Baekjoon1026.java
+++ src/main/java/baekjoon/Baekjoon1026.java
@@ -1,6 +1,8 @@
 package baekjoon;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Baekjoon1026 {
@@ -10,6 +12,15 @@ public class Baekjoon1026 {
         int sum = 0;
         for (int i = 0; i < arr1.length; i++)
             sum += arr1[i] * arr2[arr2.length-1-i];
+        return sum;
+    }
+
+    public static Integer solution2(List<Integer> list1, List<Integer> list2) {
+        List<Integer> sorted2 = quickSort(list2);
+        List<Integer> sorted1 = quickSort(list1);
+        int sum = 0;
+        for (int i = 0; i < list1.size(); i++)
+            sum += sorted1.get(i) * sorted2.get(sorted2.size()-1-i);
         return sum;
     }
 
@@ -24,6 +35,35 @@ public class Baekjoon1026 {
         for (int i = 0; i < n; i++)
             arr2[i] = sc.nextInt();
 
-        System.out.println(solution(arr1, arr2));
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            list1.add(sc.nextInt());
+
+        for (int i = 0; i < n; i++)
+            list2.add(sc.nextInt());
+        System.out.println(solution2(list1, list2));
+    }
+
+    public static List<Integer> quickSort(List<Integer> arr) {
+        if (arr.size() < 2)
+            return arr;
+
+        int pivot = arr.get(0);
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        for (int i = 1; i < arr.size(); i++)
+            if (arr.get(i) < pivot)
+                left.add(arr.get(i));
+            else
+                right.add(arr.get(i));
+
+        List<Integer> sorted = quickSort(left);
+        sorted.add(pivot);
+        sorted.addAll(quickSort(right));
+
+        return sorted;
     }
 }
