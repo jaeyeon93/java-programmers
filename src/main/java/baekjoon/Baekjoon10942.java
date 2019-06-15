9210d3b7a4048ed7b68986e6742a76144dbb2662
jaeyeon kim <foodsksms@gmail.com>
2019-06-15 20:06:26 +0900

    Solve baekjoon10942

--- src/main/java/baekjoon/Baekjoon10942.java
+++ src/main/java/baekjoon/Baekjoon10942.java
@@ -2,26 +2,28 @@ package baekjoon;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Baekjoon10942 {
-    public static Integer solution(int [] arr, int i, int j) {
-        for (int k = i-1; k < j-1; k++)
-            if (arr[k] != arr[j-k+i])
-                return 0;
-        return 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = Arrays.asList(br.readLine().split(" ")).stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String [] nums = br.readLine().split(" ");
+            System.out.println(solution(list, Integer.parseInt(nums[0]), Integer.parseInt(nums[1])));
+        }
     }
 
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [] arr = new int[n+1];
-        for (int i = 1; i <= n; i++)
-            arr[i] = sc.nextInt();
-
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++)
-            System.out.println(solution(arr, sc.nextInt(), sc.nextInt()));
-
+    public static Integer solution(List<Integer> list, int s, int e) {
+        if (s == e)
+            return 1;
+        for (int i = s-1; i < e; i++)
+            if (list.get(i) != list.get(e-i+s-2))
+                return 0;
+        return 1;
     }
 }
