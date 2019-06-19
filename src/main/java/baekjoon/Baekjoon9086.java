eb63b65710123a6a34be99e8fa499c109c8a895f
jaeyeon kim <foodsksms@gmail.com>
2019-06-19 20:17:38 +0900

    Solve baekjoon9086

--- src/main/java/baekjoon/Baekjoon9086.java
+++ src/main/java/baekjoon/Baekjoon9086.java
@@ -1,3 +1,25 @@
 package baekjoon;
 
-ㅎ
+import java.util.Scanner;
+
+public class Baekjoon9086 {
+
+    public static String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        if (str.length() == 1) {
+            sb.append(str.charAt(0));
+            sb.append(str.charAt(0));
+            return sb.toString();
+        }
+        sb.append(str.charAt(0));
+        sb.append(str.charAt(str.length()-1));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++)
+            System.out.println(solution(sc.next()));
+    }
+}
