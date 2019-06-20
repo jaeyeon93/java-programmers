e5f71f9536ff13c2baf9d4c3e5d83469852f21dd
jaeyeon kim <foodsksms@gmail.com>
2019-06-20 18:25:52 +0900

    Solve baekjoon

--- src/main/java/baekjoon/Baekjoon2501.java
+++ src/main/java/baekjoon/Baekjoon2501.java
@@ -1,17 +1,24 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Baekjoon2501 {
-    public static Integer solution(int n, int k) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++)
-            if (n % i == 0)
-                list.add(i);
-        return list.get(k-1);
+    public static Set<Integer> solution(int n) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= n/2; i++)
+            if (n % i == 0) {
+                set.add(i);
+                set.add(n/i);
+            }
+        return set;
     }
     public static void main(String[] args) {
-        System.out.println(solution(6,3));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        Set<Integer> set = solution(n);
+        Integer [] arr = set.toArray(new Integer[set.size()]);
+        Arrays.sort(arr);
+        System.out.println(arr[k-1]);
     }
 }
