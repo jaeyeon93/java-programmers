46771de113e6ed7fdf430909bb2bc36798358132
jaeyeon kim <foodsksms@gmail.com>
2019-06-26 16:56:51 +0900

    Solve baekjoon2476

--- src/main/java/baekjoon/Baekjoon2476.java
+++ src/main/java/baekjoon/Baekjoon2476.java
@@ -1,4 +1,26 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2476 {
+    public static Integer solution(int a, int b, int c) {
+        if (a == b && b == c)
+            return 10000+(a*1000);
+        else if (a != b && b != c && c != a)
+            return Math.max(Math.max(a, b), Math.max(b, c)) * 100;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int max = 0;
+        for (int i = 0; i < t; i++) {
+            int temp = solution(sc.nextInt(), sc.nextInt(), sc.nextInt());
+            if (temp > max)
+                max = temp;
+        }
+        System.out.println(max);
+
+    }
 }
