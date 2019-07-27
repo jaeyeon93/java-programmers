f0612491e820f0c1f37410eff24a762b38ad7018
jaeyeon kim <foodsksms@gmail.com>
2019-07-27 23:25:22 +0900

    Solve baekjoon1748

--- src/main/java/baekjoon/Baekjoon1748.java
+++ src/main/java/baekjoon/Baekjoon1748.java
@@ -1,4 +1,17 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon1748 {
+    public static Integer solution(int n) {
+        int num = 0;
+        for (int i = 1; i <= n; i++)
+            num += Math.log10(i)+1;
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
 }
