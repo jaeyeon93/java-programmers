d996fdf2612311c97fb17e35b67a685df804cf0d
jaeyeon kim <foodsksms@gmail.com>
2019-08-18 15:58:38 +0900

    Solve baekjjon

--- src/main/java/baekjoon/Baekjoon1212.java
+++ src/main/java/baekjoon/Baekjoon1212.java
@@ -1,4 +1,34 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon1212 {
+    public static Long octaToDecimal(String str) {
+        long result = 0;
+        String [] arr = str.split("");
+        for (int i = 0; i < arr.length; i++)
+            result += Integer.parseInt(arr[i])*Math.pow(8, arr.length-i-1);
+        return result;
+    }
+
+    public static String convertBinary(Long num) {
+        StringBuilder sb = new StringBuilder();
+
+        if (num == 0)
+            return "0";
+
+        while (num != 1) {
+            long mod = num % 2;
+            sb.append(mod);
+            num /= 2;
+        }
+        sb.append(num);
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(convertBinary(octaToDecimal(input)));
+    }
 }
