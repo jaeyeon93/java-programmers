795fce45afb7a4afbc53d2eb8fa66a871ead0a27
jaeyeon kim <foodsksms@gmail.com>
2019-12-23 13:37:56 +0900

    Baekjoon2667 conflic

--- src/main/java/baekjoon/Baekjoon2667.java
+++ src/main/java/baekjoon/Baekjoon2667.java
@@ -1,4 +1,17 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2667 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String [][] arr = new String[n][n];
+        for (int i = 0; i < n; i++) {
+            String [] input = sc.nextLine().split("");
+            for (int j = 0; j < input.length; j++)
+                arr[i][j] = input[j];
+        }
+
+    }
 }
