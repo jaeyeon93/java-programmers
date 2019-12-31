68ab7bb52fbc3adc069d3247d5dc17530cc96d7c
jaeyeon kim <foodsksms@gmail.com>
2019-12-31 20:31:05 +0900

    Github test중

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Test {
+
+    public static boolean check(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            boolean result = check(i);
+            if (result) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
