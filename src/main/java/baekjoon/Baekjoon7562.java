31432957fc33918f273c45ae3427dc92b8249317
JaeyeonKIM <foodsksms@gmail.com>
2020-01-01 21:22:31 +0900

    Solve Baekjoon7562

--- src/main/java/baekjoon/Baekjoon7562.java
+++ src/main/java/baekjoon/Baekjoon7562.java
@@ -2,6 +2,7 @@ package baekjoon;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Baekjoon7562 {
     private static final int [][] DIRECTIONS = {{1,2},{2,1},{-1,2},{-2,1},{-1,-2},{-2,-1},{1,-2},{2,-1}};
@@ -17,21 +18,24 @@ public class Baekjoon7562 {
     }
 
     public static void main(String[] args) {
-        int size = 10;
-        int [][] map = new int[size][size];
-        int [] start = {1,1};
-        int [] dst = {1,1};
-        int result = bsf(map, size, start, dst);
-        System.out.println(result);
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < t; i++) {
+            int size = Integer.parseInt(sc.nextLine());
+            int [][] map = new int[size][size];
+            String [] start = sc.nextLine().split(" ");
+            String [] dst = sc.nextLine().split(" ");
+            System.out.println(bsf(map, size, start, dst));
+        }
     }
 
-    public static int bsf(int [][] map, int size, int [] start, int [] dst) {
+    public static int bsf(int [][] map, int size, String [] start, String [] dst) {
         boolean [][] visit = new boolean[size][size];
-        int startRow = start[ROW];
-        int startCol = start[COL];
+        int startRow = Integer.parseInt(start[ROW]);
+        int startCol = Integer.parseInt(start[COL]);
         map[startRow][startCol] = 0;
-        int dstRow = dst[ROW];
-        int dstCol = dst[COL];
+        int dstRow = Integer.parseInt(dst[ROW]);
+        int dstCol = Integer.parseInt(dst[COL]);
         if (startRow == dstRow && startCol == dstCol)
             return 0;
         visit[startRow][startCol] = true;
@@ -44,7 +48,7 @@ public class Baekjoon7562 {
                 int nextCol = current.col + DIRECTION[COL];
                 if (nextRow < 0 || nextRow >= size || nextCol < 0 || nextCol >= size) continue;
                 if (visit[nextRow][nextCol]) continue;
-                System.out.println("next : " + nextRow + "," + nextCol);
+                visit[nextRow][nextCol] = true;
                 if (nextRow == dstRow && nextCol == dstCol) {
                     map[nextRow][nextCol] = map[current.row][current.col]+1;
                     return map[nextRow][nextCol];
