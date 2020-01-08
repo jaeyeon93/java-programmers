97125df0b52766471a09a623c929257a5d378e30
JaeyeonKIM <foodsksms@gmail.com>
2020-01-09 00:29:50 +0900

    2573푸는중 반례 해결을 아직 못함

--- src/main/java/baekjoon/Baekjoon2573.java
+++ src/main/java/baekjoon/Baekjoon2573.java
@@ -29,36 +29,53 @@ public class Baekjoon2573 {
             for (int col = 0; col < colSize; col++)
                 map[row][col] = Integer.parseInt(line[col]);
         }
-        int [][] convert = bsf(map, rowSize, colSize);
-        for (int i = 0; i < rowSize; i++) {
-            for (int j = 0; j < colSize; j++)
-                System.out.print(convert[i][j] + " ");
-            System.out.println();
-        }
+        int year = 0;
+        System.out.println(bsf(map, rowSize, colSize));
     }
-
-    private static int [][] bsf(int [][] map, int rowSize, int colSize) {
+    private static int bsf(int [][] map, int rowSize, int colSize) {
         boolean[][] visit = new boolean[rowSize][colSize];
         Queue<Point> q = new LinkedList<>();
+        int count = 0;
         for (int row = 0; row < rowSize; row++)
             for (int col = 0; col < colSize; col++) {
                 if (map[row][col] == 0) continue;
                 if (visit[row][col]) continue;
                 visit[row][col] = true;
+                count++;
                 q.offer(new Point(row, col));
-            }
-        while (!q.isEmpty()) {
-            Point current = q.poll();
-            for (final int [] DIRECTION : DIRECTIONS) {
-                int nextRow = current.row + DIRECTION[ROW];
-                int nextCol = current.col + DIRECTION[COL];
-                if (nextRow < 0 || nextCol < 0 || nextRow >= rowSize || nextCol >= colSize) continue;
-                if (visit[nextRow][nextCol]) continue;
-                if (map[nextRow][nextCol] == 0 && map[current.row][current.col] > 0) {
-                    map[current.row][current.col] = map[current.row][current.col] - 1;
+                while (!q.isEmpty()) {
+                    Point current = q.poll();
+                    for (final int [] DIRECTION : DIRECTIONS) {
+                        int nextRow = current.row + DIRECTION[ROW];
+                        int nextCol = current.col + DIRECTION[COL];
+                        if (nextRow < 0 || nextCol < 0 || nextRow >= rowSize || nextCol >= colSize) continue;
+                        if (visit[nextRow][nextCol]) continue;
+                        System.out.println("current : " + current.row + "," + current.col + " next : " + nextRow + "," + nextCol + " value : " + map[current.row][current.col] + " next value : " + map[nextRow][nextCol]);
+                        if (map[nextRow][nextCol] == 0 && map[current.row][current.col] > 0) {
+                            map[current.row][current.col] = map[current.row][current.col] - 1;
+                        }
+                        visit[nextRow][nextCol] = true;
+                        q.offer(new Point(nextRow, nextCol));
+                    }
                 }
             }
+
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++)
+                System.out.print(map[i][j] + " ");
+            System.out.println();
         }
-        return map;
+        return count;
     }
 }
+
+/*
+5 5
+0 0 0 0 0
+0 6 9 6 0
+0 3 3 9 0
+0 6 1 6 0
+0 0 0 0 0
+반례
+output : 4
+ */
