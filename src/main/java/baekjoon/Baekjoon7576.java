a9000a43afa7d901841a5360df02852a71e0db82
jaeyeon kim <foodsksms@gmail.com>
2020-04-05 22:09:23 +0900

    add 7576,8979

--- src/main/java/baekjoon/Baekjoon7576.java
+++ src/main/java/baekjoon/Baekjoon7576.java
@@ -1,4 +1,71 @@
 package baekjoon;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Baekjoon7576 {
+    private static int [][] DIRECTIONS = {{0,1},{1,0},{0,-1},{-1,0}};
+    private static int ROW = 0, COL = 1;
+    private static class Point {
+        int row;
+        int col;
+
+        public Point(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String [] input = sc.nextLine().split(" ");
+        int col = Integer.parseInt(input[0]);
+        int row = Integer.parseInt(input[1]);
+        int [][] map = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            String [] arr = sc.nextLine().split(" ");
+            for (int j = 0; j < arr.length; j++)
+                map[i][j] = Integer.parseInt(arr[j]);
+        }
+        System.out.println(bsf(map, row, col));
+        System.out.println("====");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++)
+                System.out.print(map[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+    public static int bsf(int [][] map, int rowSize, int colSize) {
+        boolean [][] visit = new boolean[rowSize][colSize];
+        int count = 0;
+        for (int row = 0; row < rowSize; row++)
+            for (int col = 0; col < colSize; col++) {
+                if (visit[row][col] || map[row][col] == 0) continue;
+                visit[row][col] = true;
+                Queue<Point> q = new LinkedList<>();
+                q.offer(new Point(row, col));
+                while (!q.isEmpty()) {
+                    Point current = q.poll();
+                    for (final int [] Direction : DIRECTIONS) {
+                        int nextRow = current.row + Direction[ROW];
+                        int nextCol = current.col + Direction[COL];
+                        if (nextRow < 0 || nextRow >= rowSize || nextCol < 0 || nextCol >= colSize) continue;
+                        if (visit[nextRow][nextCol]) continue;
+                        if (map[nextRow][nextCol] == -1) {
+                            continue;
+                        }
+                        if (map[nextRow][nextCol] == 0) {
+                            map[nextRow][nextCol] = 1;
+                            q.offer(new Point(nextRow, nextCol));
+                        }
+                        visit[nextRow][nextCol] = true;
+                        map[nextRow][nextCol] = map[current.row][current.col] + 1;
+                    }
+                    count++;
+                }
+            }
+        return count;
+    }
 }
--- src/main/java/baekjoon/Baekjoon8979.java
+++ src/main/java/baekjoon/Baekjoon8979.java
@@ -1,4 +1,112 @@
 package baekjoon;
 
+import java.util.*;
+
 public class Baekjoon8979 {
+    static class Country implements Comparable<Country> {
+        private int country;
+        int gold;
+        int silver;
+        int bronze;
+        int rank;
+
+        public Country(int country, int gold, int silver, int bronze) {
+            this.country = country;
+            this.gold = gold;
+            this.silver = silver;
+            this.bronze = bronze;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Country country = (Country) o;
+            return gold == country.gold &&
+                    silver == country.silver &&
+                    bronze == country.bronze;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(gold, silver, bronze);
+        }
+
+        public int getRank() {
+            return rank;
+        }
+
+        public Country setRank(int rank) {
+            this.rank = rank;
+            return this;
+        }
+
+        @Override
+        public int compareTo(Country o) {
+            if (this.gold > o.gold)
+                return 1;
+            else if (this.gold < o.gold)
+                return -1;
+
+            // silver is same;
+            if (this.silver > o.silver)
+                return 1;
+            else if (this.silver < this.silver)
+                return -1;
+
+            // bronze is same;
+            if (this.bronze > o.bronze)
+                return 1;
+            else if (this.bronze < this.bronze)
+                return -1;
+
+            // everything is same
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return "Country{" +
+                    "country=" + country +
+                    ", gold=" + gold +
+                    ", silver=" + silver +
+                    ", bronze=" + bronze +
+                    ", rank=" + rank +
+                    '}';
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String [] input = sc.nextLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        List<Country> result = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            input = sc.nextLine().split(" ");
+            int country = Integer.parseInt(input[0]);
+            int gold = Integer.parseInt(input[1]);
+            int silver = Integer.parseInt(input[2]);
+            int bronze = Integer.parseInt(input[3]);
+            result.add(new Country(country, gold, silver, bronze));
+        }
+        // 등수대로 정렬
+        Collections.sort(result);
+        Collections.reverse(result);
+        System.out.println(result);
+
+        // 등수매기기
+        int currentRank = 1;
+        for (int i = 0; i < result.size()-1; i++) {
+            if (result.get(i).equals(result.get(i+1))) {
+                currentRank++;
+                result.get(i).setRank(currentRank+1);
+                result.get(i+1).setRank(currentRank+1);
+            } else {
+                result.get(i).setRank(currentRank);
+                currentRank++;
+            }
+        }
+
+        System.out.println(result);
+    }
 }
