ebecdadcf4346cd247922cffffa4a723ac77d5a8
jaeyeon kim <foodsksms@gmail.com>
2020-04-09 23:23:08 +0900

    Solve Baekjoon2935

--- src/main/java/baekjoon/Baekjoon2935.java
+++ src/main/java/baekjoon/Baekjoon2935.java
@@ -1,4 +1,18 @@
 package baekjoon;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Baekjoon2935 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.nextLine());
+        String mod = sc.nextLine();
+        BigInteger b = new BigInteger(sc.nextLine());
+        if (mod.equals("+")) {
+            System.out.println(a.add(b));
+        } else
+            System.out.println(a.multiply(b));
+
+    }
 }
