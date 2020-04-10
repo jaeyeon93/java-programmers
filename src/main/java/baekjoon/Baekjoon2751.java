34a89b56657588491e5a0dd0d8fff493fb4f90f0
jaeyeon kim <foodsksms@gmail.com>
2020-04-10 23:47:17 +0900

    Solve Baekjoon2751

--- src/main/java/baekjoon/Baekjoon2751.java
+++ src/main/java/baekjoon/Baekjoon2751.java
@@ -4,25 +4,28 @@ import java.io.*;
 import java.util.*;
 
 public class Baekjoon2751 {
-//    public static List<Integer> sort(List<Integer> list) {
-//        if (list.size() < 2)
-//            return list;
-//
-//        int pivot = list.get(0);
-//        List<Integer> lower = new ArrayList<>();
-//        List<Integer> higher = new ArrayList<>();
-//        for (int i = 1; i < list.size(); i++) {
-//            if (list.get(i) < pivot) {
-//                lower.add(list.get(i));
-//            } else {
-//                higher.add(list.get(i));
-//            }
-//        }
-//        List<Integer> sorted = sort(lower);
-//        sorted.add(pivot);
-//        sorted.addAll(sort(higher));
-//        return sorted;
-//    }
+
+    // Quick Sort
+    public static List<Integer> sort(List<Integer> list) {
+        if (list.size() < 2)
+            return list;
+
+        int pivot = list.get(0);
+        List<Integer> lower = new ArrayList<>();
+        List<Integer> higher = new ArrayList<>();
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < pivot) {
+                lower.add(list.get(i));
+            } else {
+                higher.add(list.get(i));
+            }
+        }
+        List<Integer> sorted = sort(lower);
+        sorted.add(pivot);
+        sorted.addAll(sort(higher));
+        return sorted;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
