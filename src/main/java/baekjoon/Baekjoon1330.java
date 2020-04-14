4b6c5dbc43158a6defd0834928fe45f1cd634591
jaeyeon kim <foodsksms@gmail.com>
2020-04-14 23:42:45 +0900

    Solve Baekjoon1330

--- src/main/java/baekjoon/Baekjoon1330.java
+++ src/main/java/baekjoon/Baekjoon1330.java
@@ -1,4 +1,19 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String [] arr = input.split(" ");
+        int n = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+        if (n < m)
+            System.out.println("<");
+        else if (n == m)
+            System.out.println("==");
+        else
+            System.out.println(">");
+    }
 }
