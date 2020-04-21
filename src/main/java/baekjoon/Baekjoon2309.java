3b952d341f56171f2c1db006072cad2bb26452ba
jaeyeon kim <foodsksms@gmail.com>
2020-04-21 15:52:43 +0900

    Solve baekjoon2309

--- src/main/java/baekjoon/Baekjoon2309.java
+++ src/main/java/baekjoon/Baekjoon2309.java
@@ -7,20 +7,24 @@ public class Baekjoon2309 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 9;
-        int [] a = new int[n];
+        int [] arr = new int[n];
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-            sum += a[i];
+            arr[i] = sc.nextInt();
+            sum += arr[i];
         }
-        Arrays.sort(a);
+        Arrays.sort(arr);
+
         for (int i = 0; i < n; i++)
             for (int j = i+1; j < n; j++)
-                if (sum - a[i] - a[j] == 100)
-                    for (int k = 0; k < n; k++) {
-                        if (i == k || j == k) continue;
-                        System.out.println(a[k]);
-                    }
-                    System.exit(0);
+                if (sum - arr[i] - arr[j] == 100) {
+                    for (int k = 0; k < n; k++)
+                        if (k == i || k == j)
+                            continue;
+                        else
+                            System.out.println(arr[k]);
+                    return;
+                }
+
     }
 }
