38b037de018742d88b457f7a0e8fba34a0a72532
JaeyeonKIM <foodsksms@gmail.com>
2020-05-13 00:10:13 +0900

    solve baekjoon1182

--- src/main/java/baekjoon/Baekjoon1182.java
+++ src/main/java/baekjoon/Baekjoon1182.java
@@ -3,35 +3,37 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Baekjoon1182 {
+    private static int n;
+    private static int m;
+    private static int count;
+    private static int [] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        arr = new int[n];
         for (int i = 0; i < n; i++)
-            list.add(Integer.parseInt(st.nextToken()));
-        System.out.println(list);
+            arr[i] =(Integer.parseInt(st.nextToken()));
+        count = 0;
+        solution(0, 0);
 
-        int sum = list.get(0);
-        int count = 0;
-        int i = 0, j = 0;
-        while (i <= j && j < list.size()) {
-            if (sum < s) {
-                sum += list.get(j++);
-            } else {
-                sum -= list.get(i++);
-            }
-
-            if (sum == s)
-                count++;
-        }
+        if (m == 0) count--; // 아무것도 선택하지 않는 경우가 있다.
         System.out.println(count);
+    }
+
+    public static void solution(int i, int sum) {
+        if (i == n) {
+            if (sum == m) {
+                count++;
+            }
+            return;
+        }
+        solution(i+1, sum+arr[i]);
+        solution(i+1, sum);
     }
 }
