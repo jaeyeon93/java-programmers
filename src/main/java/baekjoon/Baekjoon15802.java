e8374bb74b50c3cb1dc29d1284292ec14254159d
jaeyeon kim <foodsksms@gmail.com>
2020-06-20 22:58:59 +0900

    Solve baekjoon15802

--- src/main/java/baekjoon/Baekjoon15802.java
+++ src/main/java/baekjoon/Baekjoon15802.java
@@ -1,4 +1,14 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon15802 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        if (input.equals("tanos"))
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
 }
