c546bfc84cf74accc35a1b4de547bea7d2e70587
jaeyeon kim <foodsksms@gmail.com>
2020-06-22 17:04:51 +0900

    Solve Baekjoon2156

--- src/main/java/baekjoon/Baekjoon2156.java
+++ src/main/java/baekjoon/Baekjoon2156.java
@@ -11,33 +11,17 @@ public class Baekjoon2156 {
 
         for (int i = 1; i <= n; i++)
             arr[i] = sc.nextInt();
-
-
-        if (n == 1) {
-            System.out.println(arr[1]);
-            return;
-        } else if (n == 2) {
-            System.out.println(arr[1] + arr[2]);
-            return;
-        } else {
-            dp[1] = arr[1];
-            dp[2] = arr[2] + dp[1];
-            dp[3] = Math.max(dp[2],Math.max(arr[1]+arr[3], arr[2]+arr[3]));
-            for (int i = 4; i <= n; i++) {
-                dp[i] = Math.max(dp[i-3] + arr[i] + arr[i-1], dp[i-2] + arr[i]);
-                dp[i] = Math.max(dp[i], dp[i-1]);
-            }
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i-2] + arr[i], arr[i]+arr[i-1]+dp[i-3]);
+            dp[i] = Math.max(dp[i], dp[i-1]);
         }
 
+        int max = Integer.MIN_VALUE;
 
-
-        int max = 0;
-        for (int i = 1 ; i<= n; i++) {
-            if (dp[i] > max)
-                max = dp[i];
-            System.out.println(dp[i]);
-        }
-
+        for (int i = 1; i <= n; i++)
+            if (dp[i] > max) max = dp[i];
         System.out.println(max);
     }
 }
