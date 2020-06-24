ca652896afb73e4776857585ad566baf16e10b53
jaeyeon kim <foodsksms@gmail.com>
2020-06-24 16:33:41 +0900

    Solve Baekjoon8979

--- src/main/java/baekjoon/Baekjoon8979.java
+++ src/main/java/baekjoon/Baekjoon8979.java
@@ -1,7 +1,9 @@
 package baekjoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Baekjoon8979 {
     private static class Nation implements Comparable<Nation> {
@@ -9,6 +11,7 @@ public class Baekjoon8979 {
         int gold;
         int silver;
         int bronze;
+        int rank;
 
         Nation(int countryNum, int gold, int silver, int bronze) {
             this.countryNum = countryNum;
@@ -17,41 +20,58 @@ public class Baekjoon8979 {
             this.bronze = bronze;
         }
 
-
         @Override
         public int compareTo(Nation o) {
-            if (this.gold > o.gold)
-                return -1;
-            else if (this.gold == o.gold) {
-                if (this.silver > o.silver)
-                    return -1;
-                else if (this.bronze == o.bronze) {
-                    if (this.bronze > o.bronze)
-                        return -1;
-                    else if (this.bronze == o.bronze)
-                        return 0;
-                    return 1;
+            if (this.gold > o.gold) return 1;
+            if (this.gold == o.gold) {
+                if (this.silver > o.silver) return 1;
+                else if (this.silver == o.silver) {
+                    if (this.bronze > o.bronze) return 1;
+                    else if (this.bronze == o.bronze) return 0;
                 }
-                return 1;
             }
-            return 1;
+            return -1;
+        }
+
+        @Override
+        public String toString() {
+            return "Nation{" +
+                    "countryNum=" + countryNum +
+                    ", gold=" + gold +
+                    ", silver=" + silver +
+                    ", bronze=" + bronze +
+                    ", rank=" + rank +
+                    '}';
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String [] input = sc.nextLine().split(" ");
-        int size = Integer.parseInt(input[0]);
-        int rank = Integer.parseInt(input[1]);
-        Nation [] arr = new Nation[size];
-        for (int i = 0; i < size; i++) {
-            String [] line = sc.nextLine().split(" ");
-            arr[i]  = new Nation(Integer.parseInt(line[0]), Integer.parseInt(line[1]), Integer.parseInt(line[2]), Integer.parseInt(line[3]));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        List<Nation> nationList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            nationList.add(new Nation(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++)
-            if (arr[i].countryNum == rank)
-                System.out.println(i+1);
 
+
+        for (int i = 0; i < n; i++) {
+            int currentRank = 1;
+            Nation current = nationList.get(i);
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                Nation next = nationList.get(j);
+                if (current.compareTo(next) == -1) currentRank++;
+            }
+            current.rank = currentRank;
+        }
+
+        for (Nation nation : nationList)
+            if (nation.countryNum == k) {
+                System.out.println(nation.rank);
+                return;
+            }
     }
 }
