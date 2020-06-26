e1b1ad00c39e670aa88734d7b0a18959e8226e85
JaeyeonKIM <foodsksms@gmail.com>
2020-06-26 23:23:50 +0900

    Solve Baekjoon10867

--- src/main/java/baekjoon/Baekjoon10867.java
+++ src/main/java/baekjoon/Baekjoon10867.java
@@ -1,29 +1,22 @@
 package baekjoon;
 
+import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Baekjoon10867 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        List<Integer> list = new ArrayList<>();
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++)
-           list.add(sc.nextInt());
-        List<Integer> result = new ArrayList<>();
-        int x = 0;
-//        while (x < list.size()) {
-//            for (Integer i : list)
-//                if (!result.contains(i))
-//                    result.add(i);
-//            x++;
-//        }
-        while (x < list.size()) {
-            result = list.parallelStream().distinct().collect(Collectors.toList());
-            x++;
-        }
+            set.add(Integer.parseInt(st.nextToken()));
+        List<Integer> result = new ArrayList<>(set);
         Collections.sort(result);
-        for (Integer i : result)
-            System.out.print(i + " ");
+        for (int i : result)
+            bw.write(i + " ");
+        bw.flush();
+        bw.close();
     }
 }
