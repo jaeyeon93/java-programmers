4d497fbc7ed49121f9a46cc07cb6c8fc10fa341f
JaeyeonKIM <foodsksms@gmail.com>
2020-07-16 22:37:28 +0900

    Solve Baekjoon1620 using binary search

--- src/main/java/baekjoon/Baekjoon1620.java
+++ src/main/java/baekjoon/Baekjoon1620.java
@@ -3,12 +3,11 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Baekjoon1620 {
-    private static List<String> arr = new ArrayList<>();
+    private static Map<Integer, Monster> map = new HashMap<>();
+    private static List<Monster> monsters = new ArrayList<>();
     private static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,8 +15,12 @@ public class Baekjoon1620 {
 
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++)
-            arr.add(br.readLine());
+        for (int i = 0; i < n; i++) {
+            Monster monster = new Monster(i+1, br.readLine());
+            monsters.add(monster);
+            map.put(i+1, monster);
+        }
+        Collections.sort(monsters);
         for (int i = 0; i < m; i++)
             solution(br.readLine());
     }
@@ -25,11 +28,46 @@ public class Baekjoon1620 {
     private static void solution(String input) {
         try {
             int index = Integer.parseInt(input);
-            System.out.println(arr.get(index-1));
+            System.out.println(map.get(index).name);
             return;
         } catch (NumberFormatException e) {
-            System.out.println(arr.indexOf(input+1));
+            // Binary Search
+            int left = 0;
+            int right = monsters.size()-1;
+            int mid = (left + right) / 2;
+            int count = 0;
+            while (right >= left || count < 10) {
+                Monster next = monsters.get(mid);
+                count++;
+                if (next.name.equals(input)) {
+                    System.out.println(next.pocketNum);
+                    break;
+                }
+
+                if (next.name.compareTo(input) > 0) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+                mid = (right + left) / 2;
+            }
+
             return;
+        }
+    }
+
+    private static class Monster implements Comparable<Monster> {
+        int pocketNum;
+        String name;
+
+        public Monster(int pocketNum, String name) {
+            this.pocketNum = pocketNum;
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(Monster o) {
+            return this.name.compareTo(o.name);
         }
     }
 }
