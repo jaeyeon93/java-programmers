9106ab9a7833b0aada44e5ee06ff202de26bde13
JaeyeonKIM <foodsksms@gmail.com>
2020-07-18 22:54:02 +0900

    Sovle Baekjoon1748

--- src/main/java/baekjoon/Baekjoon1748.java
+++ src/main/java/baekjoon/Baekjoon1748.java
@@ -3,15 +3,19 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Baekjoon1748 {
-    public static Integer solution(int n) {
-        int num = 0;
-        for (int i = 1; i <= n; i++)
-            num += Math.log10(i)+1;
-        return num;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int result = 0;
+        int length = 10;
+        int digit = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i == length) {
+                digit++;
+                length *= 10;
+            }
+            result += digit;
+        }
+        System.out.println(result);
     }
 }
