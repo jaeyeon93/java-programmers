5fe2c8be49b65eae4be09c2904458244149aeaae
jaeyeon kim <foodsksms@gmail.com>
2020-08-13 20:50:27 +0900

    solve baekjoon 2839

--- src/main/java/baekjoon/Baekjoon2839.java
+++ src/main/java/baekjoon/Baekjoon2839.java
@@ -1,4 +1,28 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while (true) {
+            if (n % 5 == 0) {
+                int div = n / 5;
+                count += div;
+                n %= 5;
+            } else {
+                n -= 3;
+                count++;
+            }
+            if (n <= 0)
+                break;
+        }
+
+        if (n == 0)
+            System.out.println(count);
+        else if (n < 0)
+            System.out.println(-1);
+    }
 }
