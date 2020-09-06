cbe9d54361092c13c0dc23d0f0f1f4c95afbb3f1
JaeyeonKIM <foodsksms@gmail.com>
2020-09-06 23:36:11 +0900

    Solve ALgorithm

--- src/test/java/SuperTest1.java
+++ src/test/java/SuperTest1.java
@@ -8,24 +8,7 @@ public class SuperTest1 {
 
     @Test
     public void 집합이정상적으로만들어지는() {
-        List<Integer> list1 = new ArrayList<>();
-        for (int i = 1; i <= 300; i++) {
-            if (i % 6 == 0) list1.add(i);
-            if (i % 8 == 0) list1.add(i);
-        }
-        TreeSet<Integer> set = new TreeSet<>(list1);
-        System.out.println(set);
-        Iterator<Integer> itr = set.iterator();
-        int count = 0;
-        int result = 0;
-        while (itr.hasNext()) {
-            int num = itr.next();
-            count++;
-            if (count == 8) {
-                result = num;
-                break;
-            }
-        }
-        assertThat(result, is(32));
+        String input = "12,356";
+        System.out.println(Integer.parseInt(input.replaceAll(",","")));
     }
 }
